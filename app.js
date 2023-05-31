3cda38ce63cee64a27333a6cafa4cedf4a060b9e
Mohammed Tifour <killuazoldyckt4@gmail.com>
2023-05-31 22:43:58 +0100

    config eslint and prettier

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const express = require('express');
+
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/userRouter');
+
 const app = express();
 
 app.use('/api/v1/user', userRouter);
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DB_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    console.log('database connection established')
-  );
+  .then(() => console.log('database connection established'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
-const userController = require('../controllers/userController.js');
+const userController = require('../controllers/userController');
+
 router.route('/').get(userController.getUser);
 
 module.exports = router;
